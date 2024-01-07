f3e185184a144910a26f66f3a6cdaddf0cfcfca7
Raghav Agrawal <raghav.agrawal@spinny.com>
2024-01-07 23:57:30 +0530

    global search working now

--- routes/api/user.js
+++ routes/api/user.js
@@ -18,6 +18,17 @@ router.get('/users', async (req, res) => {
 	}
 })
 
+router.get('/users/:name', async (req, res) => {
+	try {
+		let users = await usersData.find({ name: { $regex: req.params.name, $options: 'i' } });
+		if (!users) throw Error('No users');
+		res.status(200).json({ data: users, msg: "Users Fetch Successfully" });
+	}
+	catch (err) {
+		res.status(400).json({ msg: err })
+	}
+})
+
 router.get('/chips', async (req, res) => {
 	try {
 		let chips = await chipsData.find();
@@ -43,15 +54,15 @@ router.get('/comments/:pk', async (req, res) => {
 // Update comments for a user
 router.patch('/addComment/:pk', async (req, res) => {
 	try {
-		const user = await commentsData.findOneAndUpdate(
+		const comments = await commentsData.findOneAndUpdate(
 			{ pk: req.params.pk }, // Find by the provided ID
 			{ $set: { comments: req.body } }, // Set the fields to update
 			{
 				returnOriginal: false // Return the updated document (optional) 
 			}
 		);
-		if (!user) throw Error('Something went wrong while adding the comment');
-		res.status(200).json({ data: user, msg: "comment added successfully" });
+		if (!comments) throw Error('Something went wrong while adding the comment');
+		res.status(200).json({ data: comments, msg: "comment added successfully" });
 	}
 	catch (err) {
 		res.status(400).json({ msg: err })
